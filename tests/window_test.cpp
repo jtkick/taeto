6d5cc0aab8fbb895505d1a1d07e8c439a5753e1a
Jared Kick <jaredkick@gmail.com>
2025-02-11 22:17:23 -0500

    Fixed window test.

--- tests/window_test.cpp
+++ tests/window_test.cpp
@@ -1,53 +1,120 @@
-// #include "assets/scenes/window_test.hpp"
+#ifndef ASSETS_SCENES_WINDOW_TEST_H_
+#define ASSETS_SCENES_WINDOW_TEST_H_
 
-// #include <memory>
-// #include <vector>
+#include <memory>
 
-// #include "engine.hpp"
-// #include "assets/lights/point_light.hpp"
-// #include "assets/sprites/checkerboard.hpp"
-// #include "components/display_pixel.hpp"
-// #include "systems/window_system/horizontal_layout.hpp"
-// #include "systems/window_system/widget.hpp"
-// #include "systems/window_system/text_box.hpp"
+#include "taeto/components/render_pixel.hpp"
+#include "taeto/objects/sprites/rectangle.hpp"
+#include "taeto/engine.hpp"
+#include "taeto/scenes/scene.hpp"
+#include "taeto/shaders/checkerboard.hpp"
+#include "taeto/widgets/horizontal_layout.hpp"
+#include "taeto/widgets/widget.hpp"
+#include "taeto/widgets/text_box.hpp"
 
-// namespace taeto
-// {
+class TestWindow : public taeto::Widget
+{
+public:
+    TestWindow()
+    {
+        hl_.add_widget(std::make_shared<taeto::TextBox>("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."), 1);
+        hl_.add_widget(std::make_shared<taeto::TextBox>("THIS IS A TEST"), 1);
+    };
 
-// // WindowTest::WindowTest()
-// // {
-// //     cb_ = std::make_shared<taeto::Checkerboard>(glm::uvec2(1000, 1000), glm::vec3{1.0, 1.0, 1.0}, false);
-// //     cb_->position({
-// //         -((double)cb_->width()/2),
-// //         -((double)cb_->height()/2),
-// //         -10});
+    ~TestWindow() {};
 
-// //     w_ = std::make_shared<Widget>("main");
-// //     w_->position({20, 5});
-// //     w_->size({20, 2});
+    void size(glm::uvec2 v)
+    {
+        size_ = v;
+        hl_.size(v - glm::uvec2(4, 2));
+    };
 
-// //     DisplayPixelFrame t({14, 1});
-// //     std::string tt = "this is a test";
-// //     for (int i = 0; i < t.width(); i++)
-// //     {
-// //         t.at({i, 0}).c = tt.at(i);
-// //         t.at({i, 0}).bg_color = {0, 0, 0, 0.5};
-// //     }
-// //     tb_ = std::make_shared<TextBox>();
-// //     tb_->text(t);
-// //     tb_->position({-2, 10});
+    glm::uvec2 size()
+    {
+        return size_;
+    }
 
-// // }
+    taeto::DisplayPixelFrame render()
+    {
+        taeto::DisplayPixelFrame result(size());
+        for (int y = 0; y < result.height(); y++)
+        {
+            for (int x = 0; x < result.width(); x++)
+            {
+                result.at({x, y}).bg_color = glm::vec4(0.0, 0.0, 0.0, 0.5);
 
-// // WindowTest::~WindowTest()
-// // {
+                if (y == 0 || y == result.height()-1)
+                    result.at({x, y}).c = '#';
+                else if (x < 2 || x > result.width()-3)
+                    result.at({x, y}).c = '#';
 
-// // }
+            }
+        }
 
-// // void WindowTest::load()
-// // {
-// //     taeto::load_sprite(cb_);
-// //     taeto::load_widget(tb_);
-// // }
+        taeto::DisplayPixelFrame l = hl_.render();
+        result.apply(
+            l,
+            glm::uvec2(2, 1),
+            false,
+            [](taeto::DisplayPixel& a, taeto::DisplayPixel& b)->taeto::DisplayPixel&
+            {
+                a.c = b.c;
+                a.fg_color = taeto::mix_colors(a.fg_color, b.fg_color);
+                a.bg_color = taeto::mix_colors(a.bg_color, b.bg_color);
+                a.bold = b.bold;
+                a.italic = b.italic;
+                a.underline = b.underline;
+                a.strikethrough = b.strikethrough;
+                return a;
+            }
+        );
+        return result;
+        
+    };
 
-// }   // namespace taeto
+protected:
+    taeto::HorizontalLayout hl_;
+};
+
+class WindowTest : public taeto::Scene
+{
+public:
+    WindowTest()
+    {
+        cb_ = std::make_shared<taeto::Rectangle>(
+            glm::uvec2(1000, 1000),
+            taeto::RenderPixel(' ', glm::vec4(), glm::vec4(1.0, 1.0, 1.0, 1.0), false));
+        cb_->position({
+            -((double)cb_->width()/2),
+            -((double)cb_->height()/2),
+            -10});
+        cb_->add_shader(std::make_shared<taeto::shaders::Checkerboard>());
+        tw_ = std::make_shared<TestWindow>();
+        tw_->size({50, 10});
+        tw_->position({40, 15});
+    }
+
+    ~WindowTest() {};
+
+    void load()
+    {
+        taeto::load_sprite(cb_);
+        // taeto::load_widget(tb_);
+        taeto::load_widget(tw_);
+    }
+
+private:
+    std::shared_ptr<taeto::Rectangle> cb_;
+    // std::shared_ptr<Widget> w_;
+    // std::shared_ptr<TextBox> tb_;
+    std::shared_ptr<TestWindow> tw_;
+};
+
+int main()
+{
+    taeto::set_debug_mode(true);
+    taeto::load_scene(std::make_shared<WindowTest>());
+    taeto::run();
+}
+
+#endif  // ASSETS_SCENES_WINDOW_TEST_H_
